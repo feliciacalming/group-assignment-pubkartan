6003bb995aa0948730b339900dd26e0be8853f60
adamovd <adam.ov.danielsson@gmail.com>
2023-02-28 11:39:58 +0100

    reviewschema added

--- src/middleware/validation/validationSchemas.js
+++ src/middleware/validation/validationSchemas.js
@@ -1,15 +1,25 @@
-const { body } = require('express-validator')
+const { body } = require("express-validator");
 
 exports.registerSchema = [
-	body('email').isEmail().withMessage('You must provide a valid email address'),
-	body('password')
-		.not()
-		.isEmpty()
-		.isLength({ min: 6 })
-		.withMessage('You must provide a password that is at least 6 characters long'),
-]
+  body("email").isEmail().withMessage("You must provide a valid email address"),
+  body("password")
+    .not()
+    .isEmpty()
+    .isLength({ min: 5 })
+    .withMessage(
+      "You must provide a password that is at least 5 characters long"
+    ),
+];
 
 exports.loginSchema = [
-	body('email').isEmail().withMessage('You must provide a valid email address'),
-	body('password').not().isEmpty().withMessage('You must provide a password'),
-]
+  body("email").isEmail().withMessage("You must provide a valid email address"),
+  body("password").not().isEmpty().withMessage("You must provide a password"),
+];
+
+exports.reviewSchema = [
+  body("review")
+    .not()
+    .isEmpty()
+    .isLength({ min: 5, max: 250 })
+    .withMessage("Your review must be between 5 and 250 characters"),
+];
