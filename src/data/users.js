9b62bef18afa1ff0cbc0842df92754f3a928dcc8
Felicia Calming <felicia.calming@gmail.com>
2023-03-01 15:18:22 +0100

    fixing seedDb

--- src/data/users.js
+++ src/data/users.js
@@ -2,24 +2,39 @@ const { userRoles, pubRoles } = require("../constants/users");
 
 exports.users = [
   {
+    username: "felle",
     email: "felicia.calming@gmail.com",
     password: "banan",
     role: userRoles.ADMIN,
+    created_at: "2023-02-23",
   },
 
   {
+    username: "adam",
     email: "adam.danielsson@gmail.com",
     password: "banan",
     role: userRoles.USER,
+    created_at: "2023-02-24",
   },
   {
+    username: "johan",
     email: "johan.strang@gmail.com",
     password: "banan",
     role: userRoles.USER,
+    created_at: "2023-02-25",
   },
   {
+    username: "per",
     email: "per.berge@gmail.com",
     password: "banan",
     role: userRoles.USER,
+    created_at: "2023-02-28",
+  },
+  {
+    username: "max",
+    email: "max.karlsson@gmail.com",
+    password: "banan",
+    role: userRoles.USER,
+    created_at: "2023-02-28",
   },
 ];
--- src/database/seedDb.js
+++ src/database/seedDb.js
@@ -17,25 +17,29 @@ const seedPubsDb = async () => {
     await sequelize.query(`
     CREATE TABLE IF NOT EXISTS user (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL, 
       email TEXT NOT NULL,
       password TEXT NOT NULL,
-      role TEXT NOT NULL DEFAULT "USER"
+      role TEXT NOT NULL DEFAULT "USER",
+      created_at TEXT NOT NULL
     );
    `);
 
-    let userInsertQuery = `INSERT INTO user (email, password, role) VALUES `;
+    let userInsertQuery = `INSERT INTO user (username, email, password, role, created_at) VALUES `;
 
     let userInsertQueryVariables = [];
 
     for (let i = 0; i < users.length; i++) {
+      let username = users[i].username;
       let email = users[i].email;
       let password = users[i].password;
       let role = users[i].role;
+      let created_at = users[i].created_at;
 
       const salt = await bcrypt.genSalt(10);
       const hashedpassword = await bcrypt.hash(password, salt);
 
-      let values = `("${email}", "${hashedpassword}", "${role}")`;
+      let values = `("${username}", "${email}", "${hashedpassword}", "${role}", "${created_at}")`;
       userInsertQuery += values;
       if (i < users.length - 1) userInsertQuery += ", ";
     }
@@ -43,6 +47,40 @@ const seedPubsDb = async () => {
     userInsertQuery += ";";
 
     await sequelize.query(userInsertQuery);
+
+    //city
+
+    await sequelize.query(`
+    CREATE TABLE IF NOT EXISTS city (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      city_name TEXT NOT NULL
+    );
+   `);
+
+    let cityInsertQuery = "INSERT INTO city (city_name) VALUES ";
+
+    let cityInsertQueryVariables = [];
+
+    console.log(cities);
+    cities.forEach((city, index, array) => {
+      let string = "(";
+      for (let i = 1; i < 2; i++) {
+        string += `$${cityInsertQueryVariables.length + i}`;
+        if (i < 1) string += ",";
+      }
+      cityInsertQuery += string + ")";
+      if (index < array.length - 1) cityInsertQuery += ",";
+
+      const variables = [city.city_name];
+      cityInsertQueryVariables = [...cityInsertQueryVariables, ...variables];
+      console.log(cityInsertQueryVariables);
+    });
+
+    cityInsertQuery += ";";
+
+    await sequelize.query(cityInsertQuery, {
+      bind: cityInsertQueryVariables,
+    });
 
     /************ Pubs ***********/
 
@@ -52,25 +90,29 @@ const seedPubsDb = async () => {
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           name TEXT NOT NULL,
           address TEXT NOT NULL,
-          city TEXT NOT NULL,
+          fk_city_id INTEGER,
           description TEXT,
           opening_hours TEXT,
           happy_hour TEXT,
           beer_price TEXT,
-          webpage TEXT
+          webpage TEXT,
+          fk_user_id INTEGER,
+
+          FOREIGN KEY(fk_city_id) REFERENCES city(id),
+          FOREIGN KEY(fk_user_id) REFERENCES user(id)
         );
        `);
 
     let pubInsertQuery =
-      "INSERT INTO pub (name, address, city, description, opening_hours, happy_hour, beer_price, webpage) VALUES ";
+      "INSERT INTO pub (name, address, fk_city_id, description, opening_hours, happy_hour, beer_price, webpage, fk_user_id) VALUES ";
 
     let pubInsertQueryVariables = [];
 
     pubs.forEach((pub, index, array) => {
       let string = "(";
-      for (let i = 1; i < 9; i++) {
+      for (let i = 1; i < 10; i++) {
         string += `$${pubInsertQueryVariables.length + i}`;
-        if (i < 8) string += ",";
+        if (i < 9) string += ",";
       }
       pubInsertQuery += string + ")";
       if (index < array.length - 1) pubInsertQuery += ",";
@@ -78,12 +120,13 @@ const seedPubsDb = async () => {
       const variables = [
         pub.name,
         pub.address,
-        pub.city,
+        pub.fk_city_id,
         pub.description,
         pub.opening_hours,
         pub.happy_hour,
         pub.beer_price,
         pub.webpage,
+        pub.fk_user_id,
       ];
       pubInsertQueryVariables = [...pubInsertQueryVariables, ...variables];
     });
@@ -142,40 +185,6 @@ const seedPubsDb = async () => {
 
     await sequelize.query(reviewInsertQuery, {
       bind: reviewInsertQueryVariables,
-    });
-
-    //city
-
-    await sequelize.query(`
-    CREATE TABLE IF NOT EXISTS city (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      city_name TEXT NOT NULL
-    );
-   `);
-
-    let cityInsertQuery = "INSERT INTO city (city_name) VALUES ";
-
-    let cityInsertQueryVariables = [];
-
-    console.log(cities);
-    cities.forEach((city, index, array) => {
-      let string = "(";
-      for (let i = 1; i < 2; i++) {
-        string += `$${cityInsertQueryVariables.length + i}`;
-        if (i < 1) string += ",";
-      }
-      cityInsertQuery += string + ")";
-      if (index < array.length - 1) cityInsertQuery += ",";
-
-      const variables = [city.city_name];
-      cityInsertQueryVariables = [...cityInsertQueryVariables, ...variables];
-      console.log(cityInsertQueryVariables);
-    });
-
-    cityInsertQuery += ";";
-
-    await sequelize.query(cityInsertQuery, {
-      bind: cityInsertQueryVariables,
     });
 
     console.log("Database successfully populated with data...");
