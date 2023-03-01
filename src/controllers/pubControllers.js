eb44c65e1a7722e21f3f1676e32241717902e8c2
adamovd <adam.ov.danielsson@gmail.com>
2023-03-01 11:03:01 +0100

    added create pub

--- src/controllers/pubControllers.js
+++ src/controllers/pubControllers.js
@@ -1,7 +1,9 @@
+const { QueryTypes } = require("sequelize");
+const { sequelize } = require("../database/config");
+
 exports.getAllPubs = async (req, res) => {
-  let query;
-  let options = {};
-  query = `SELECT * FROM pubs`;
+  const [pubs, metadata] = await sequelize.query(`SELECT * FROM pub`);
+  return res.json(pubs);
 };
 
 exports.getPubById = async (req, res) => {
@@ -9,7 +11,39 @@ exports.getPubById = async (req, res) => {
 };
 
 exports.createNewPub = async (req, res) => {
-  console.log("hej");
+  const {
+    name,
+    address,
+    city,
+    description,
+    opening_hours,
+    happy_hour,
+    beer_price,
+    webpage,
+  } = req.body;
+
+  const [newPubId] = await sequelize.query(
+    `INSERT INTO pub (name, address, city, description, opening_hours, happy_hour, beer_price, webpage) VALUES ($name, $address, $city, $description, $opening_hours, $happy_hour, $beer_price, $webpage);`,
+    {
+      bind: {
+        name: name,
+        address: address,
+        city: city,
+        description: description,
+        opening_hours: opening_hours,
+        happy_hour: happy_hour,
+        beer_price: beer_price,
+        webpage: webpage,
+      },
+      type: QueryTypes.INSERT,
+    }
+  );
+  return res
+    .setHeader(
+      "Location",
+      `${req.protocol}://${req.headers.host}/api/v1/pubs/${newPubId}`
+    )
+    .sendStatus(201);
 };
 
 exports.updatePub = async (req, res) => {
