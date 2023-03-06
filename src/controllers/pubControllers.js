ce7b9941e8e19135c1e162a4ea39623e9ea4b5d0
Felicia Calming <felicia.calming@gmail.com>
2023-03-06 22:54:24 +0100

    trying some things to avoid duplicates when creating new pub

--- src/controllers/pubControllers.js
+++ src/controllers/pubControllers.js
@@ -3,6 +3,7 @@ const {
   NotFoundError,
   UnauthenticatedError,
   UnauthorizedError,
+  BadRequestError,
 } = require("../utils/errors");
 const { sequelize } = require("../database/config");
 const { userRoles } = require("../constants/users");
@@ -67,14 +68,50 @@ exports.createNewPub = async (req, res) => {
   } = req.body;
 
   const userId = req.user.userId;
+  let cityId; //deklarerar variabeln utanför if-satserna så den går att ändra där inne
 
+  //kollar ifall puben redan finns i den staden och på den adressen
+  const [checkForExistingPub] = await sequelize.query(
+    `SELECT name AS pub_name, address AS pub_address, city.city_name, city.id AS city_id FROM city LEFT JOIN pub ON pub.name = $name AND pub.fk_city_id = city.id AND address = $address WHERE city.city_name = $city`,
+    {
+      bind: {
+        name: name,
+        city: city,
+        address: address,
+      },
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  //checkForExistingPub blir null om staden inte finns i city-tabellen, och då lägger vi här till staden i city-tabellen innan vi skickar in hela puben där nere.
+  if (!checkForExistingPub) {
+    const [newCity] = await sequelize.query(
+      `INSERT INTO city (city_name) VALUES ($city);`,
+      { bind: { city: city }, type: QueryTypes.INSERT }
+    );
+
+    cityId = newCity.id; //tar ut id:t som skapats för den nya staden
+  } else if (
+    checkForExistingPub.pub_name &&
+    checkForExistingPub.city_name &&
+    checkForExistingPub.pub_address
+  ) {
+    throw new BadRequestError(
+      "Den här puben finns redan i den här staden på den här adressen!"
+    );
+  } else {
+    //om vi inte behöver lägga till en ny stad eller om exakt den puben inte finns, tar vi id:t från staden i checkForExistingPub och skickar med till nya puben här nere:
+    cityId = checkForExistingPub.city_id;
+  }
+
+  //ibland när man försöker skapa en helt ny pub i en ny stad osv klagar postman på att bind parameter fk_city_id inte har value i given object blabla men funkar om man försöker en gång till :)
   const [newPubId] = await sequelize.query(
     `INSERT INTO pub (name, address, fk_city_id, description, opening_hours, happy_hour, beer_price, webpage, fk_user_id) VALUES ($name, $address, $fk_city_id, $description, $opening_hours, $happy_hour, $beer_price, $webpage, $fk_user_id);`,
     {
       bind: {
         name: name,
         address: address,
-        fk_city_id: city,
+        fk_city_id: cityId,
         description: description,
         opening_hours: opening_hours,
         happy_hour: happy_hour,
@@ -86,6 +123,7 @@ exports.createNewPub = async (req, res) => {
     }
   );
 
+  console.log(newPubId);
   return res
     .setHeader(
       "Location",
