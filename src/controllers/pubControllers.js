aa07835fb871fa553732fe4013d99d78072f009b
Felicia Calming <felicia.calming@gmail.com>
2023-03-07 11:26:29 +0100

    fixed capitalization and trim on getAllPubs and createNewPub

--- src/controllers/pubControllers.js
+++ src/controllers/pubControllers.js
@@ -19,11 +19,12 @@ exports.getAllPubs = async (req, res) => {
       { bind: { limit } }
     );
   } else {
+    city = city.trim();
     city = city[0].toUpperCase() + city.substring(1).toLowerCase();
 
     pubs = await sequelize.query(
       `SELECT pub.name, pub.address, pub.description, pub.opening_hours, pub.happy_hour, pub.beer_price, pub.webpage, city.city_name FROM pub LEFT JOIN city ON city.id = pub.fk_city_id WHERE city.city_name = $city LIMIT $limit;`,
-      { bind: { city: city.trim(), limit } }
+      { bind: { city: city, limit } }
     );
   }
 
@@ -36,6 +37,8 @@ exports.getPubById = async (req, res) => {
     bind: { pubId },
     type: QueryTypes.SELECT,
   });
+
+  console.log(pub);
 
   const pubReviews = await sequelize.query(
     `SELECT review.id, review.review, review.rating, review.created_at, user.username AS username FROM review JOIN user ON pub.id = review.fk_pub_id JOIN pub ON user.id = review.fk_user_id WHERE pub.id = $pubId;`,
@@ -68,29 +71,33 @@ exports.createNewPub = async (req, res) => {
   } = req.body;
 
   const userId = req.user.userId;
-  let cityId; //deklarerar variabeln utanför if-satserna så den går att ändra där inne
 
-  //kollar ifall puben redan finns i den staden och på den adressen
+  let cityId;
+  let city_name = city.trim();
+  city_name = city_name[0].toUpperCase() + city_name.substring(1).toLowerCase();
+
   const [checkForExistingPub] = await sequelize.query(
-    `SELECT name AS pub_name, address AS pub_address, city.city_name, city.id AS city_id FROM city LEFT JOIN pub ON pub.name = $name AND pub.fk_city_id = city.id AND address = $address WHERE city.city_name = $city`,
+    `SELECT name AS pub_name, address AS pub_address, city.city_name, city.id AS city_id 
+    FROM city 
+    LEFT JOIN pub ON pub.name = $name AND pub.fk_city_id = city.id AND address = $address 
+    WHERE city.city_name = $city`,
     {
       bind: {
         name: name,
-        city: city,
+        city: city_name,
         address: address,
       },
       type: QueryTypes.SELECT,
     }
   );
 
-  //checkForExistingPub blir null om staden inte finns i city-tabellen, och då lägger vi här till staden i city-tabellen innan vi skickar in hela puben där nere.
   if (!checkForExistingPub) {
     const [newCity] = await sequelize.query(
       `INSERT INTO city (city_name) VALUES ($city);`,
-      { bind: { city: city }, type: QueryTypes.INSERT }
+      { bind: { city: city_name }, type: QueryTypes.INSERT }
     );
 
-    cityId = newCity.id; //tar ut id:t som skapats för den nya staden
+    cityId = newCity;
   } else if (
     checkForExistingPub.pub_name &&
     checkForExistingPub.city_name &&
@@ -100,13 +107,12 @@ exports.createNewPub = async (req, res) => {
       "Den här puben finns redan i den här staden på den här adressen!"
     );
   } else {
-    //om vi inte behöver lägga till en ny stad eller om exakt den puben inte finns, tar vi id:t från staden i checkForExistingPub och skickar med till nya puben här nere:
     cityId = checkForExistingPub.city_id;
   }
 
-  //ibland när man försöker skapa en helt ny pub i en ny stad osv klagar postman på att bind parameter fk_city_id inte har value i given object blabla men funkar om man försöker en gång till :)
   const [newPubId] = await sequelize.query(
-    `INSERT INTO pub (name, address, fk_city_id, description, opening_hours, happy_hour, beer_price, webpage, fk_user_id) VALUES ($name, $address, $fk_city_id, $description, $opening_hours, $happy_hour, $beer_price, $webpage, $fk_user_id);`,
+    `INSERT INTO pub (name, address, fk_city_id, description, opening_hours, happy_hour, beer_price, webpage, fk_user_id) 
+    VALUES ($name, $address, $fk_city_id, $description, $opening_hours, $happy_hour, $beer_price, $webpage, $fk_user_id);`,
     {
       bind: {
         name: name,
@@ -123,7 +129,6 @@ exports.createNewPub = async (req, res) => {
     }
   );
 
-  console.log(newPubId);
   return res
     .setHeader(
       "Location",
